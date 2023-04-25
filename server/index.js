51b80a9179ae4f947c3078c7415ec1425a145d3a
Jeeyk1 <nooojake@yahoo.com>
2023-04-25 11:23:52 +0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const conn = require("./config/db");
+const enrolleesRouter = require("./routes/enrollmentRoutes");
+const bodyParser = require("body-parser");
+const userRouter = require("./routes/usersRoutes");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// use the json middleware to parse JSON request bodies
+app.use(bodyParser.json());
+
+conn();
+app.use("/api/auth", userRouter);
+app.use("/api/enrollees", enrolleesRouter);
+const PORT = 3001 || 3002;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}....`);
+});
---
+++ server/models/EnrollmentModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const EnrollmentSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 20,
+  },
+  lastName: {
+    type: String,
+    required: true,
+
+    max: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    min: 8,
+  },
+
+  isApproved: {
+    type: Boolean,
+    default: false,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Enrollees", EnrollmentSchema);
